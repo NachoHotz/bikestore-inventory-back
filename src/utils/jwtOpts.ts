c3d4c4a4f7f076f6882ddc03dd7bf8f09a11e2b1
nachohotz <juanhotz611@gmail.com>
2022-09-30 22:47:27 -0300

    FEAT: decode jwt opts. Fix access token exp

--- src/utils/jwtOpts.ts
+++ src/utils/jwtOpts.ts
@@ -6,9 +6,18 @@ const { JWT_ACCESS_TOKEN_SECRET, JWT_ACCESS_TOKEN_EXP, JWT_REFRESH_TOKEN_SECRET,
 
 export const jwtOptsSign = {
   access(dataStoredInToken: DataStoredInToken) {
-    return jwt.sign(dataStoredInToken, JWT_ACCESS_TOKEN_SECRET, { expiresIn: JWT_REFRESH_TOKEN_EXP });
+    return jwt.sign(dataStoredInToken, JWT_ACCESS_TOKEN_SECRET, { expiresIn: JWT_ACCESS_TOKEN_EXP });
   },
   refresh(dataStoredInToken: DataStoredInToken) {
     return jwt.sign(dataStoredInToken, JWT_REFRESH_TOKEN_SECRET, { expiresIn: JWT_REFRESH_TOKEN_EXP });
+  }
+};
+
+export const jwtOptsDecode = {
+  access(token: string) {
+    return jwt.verify(token, JWT_ACCESS_TOKEN_SECRET) as DataStoredInToken;
+  },
+  refresh(token: string) {
+    return jwt.verify(token, JWT_REFRESH_TOKEN_SECRET) as DataStoredInToken;
   }
 };
