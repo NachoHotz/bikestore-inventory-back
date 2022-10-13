c775dfaa206d786d30d5aec11dcbe866716cc670
nachohotz <juanhotz611@gmail.com>
2022-10-13 12:24:45 -0300

    task(prisma): connection success or error log

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import app from './src/app';
-import { envConfig, logger } from './src/config';
+import { envConfig, logger, prisma } from './src/config';
 
 const { API_PORT, NODE_ENV } = envConfig;
 
 app.listen(API_PORT || 3001, () => {
-  logger.info(`Server running on PORT ${API_PORT || 3001} in ${NODE_ENV} mode`);
+  prisma.$connect().then(() => {
+    logger.info('Database connection successfull');
+    logger.info(`Server running on PORT ${API_PORT || 3001} in ${NODE_ENV} mode`);
+  }).catch((error) => {
+    logger.error(`Database connection error: ${error}`);
+    process.exit(0);
+  });
 });
 
