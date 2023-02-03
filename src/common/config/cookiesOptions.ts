61f6089a6b9566a50d03ea14edc17e1f812bdee5
Nacho Hotz <juanhotz611@gmail.com>
2023-02-03 16:44:50 -0300

    fix: expires option in cookies

--- src/common/config/cookiesOptions.ts
+++ src/common/config/cookiesOptions.ts
@@ -6,13 +6,11 @@ const { JWT_ACCESS_TOKEN_EXP, JWT_REFRESH_TOKEN_EXP } = envConfig;
 export const SessionCookieOpts: CookieOptions = {
   httpOnly: true,
   sameSite: 'lax',
-  secure: true,
-  expires: 3600000
+  secure: true
 };
 
 export const RefreshCookieOpts: CookieOptions = {
   httpOnly: true,
   sameSite: 'lax',
-  secure: true,
-  expires: 86400000
+  secure: true
 };
